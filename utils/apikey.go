315c69f85fc57ff791445513703d4af9e2bc8378
Luke Parrish <lsparrish@users.noreply.github.com>
2023-03-28 21:55:56 -0600

    Update apikey.go

--- utils/apikey.go
+++ utils/apikey.go
@@ -1,37 +1,58 @@
 package utils
 
 import (
-    "bufio"
-    "log"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 func GetApiKey() string {
-    file, err := os.Open(".env")
-    if err == nil {
-        defer file.Close()
-        return getApiKeyFromScanner(bufio.NewScanner(file))
-    } else {
-        log.Print(err)
-    }
-
-    log.Print("Enter OpenAI API key: ")
-    apiKey := getApiKeyFromScanner(bufio.NewScanner(os.Stdin))
-    if apiKey != "" {
-        if err := os.WriteFile(".env", []byte("OPENAI_API_KEY="+apiKey), 0600); err != nil {
-            log.Fatal(err)
-        }
-    }
-    return apiKey
-    
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	return getApiKeyWithReaderWriter(reader, writer)
 }
 
-func getApiKeyFromScanner(scanner *bufio.Scanner) string {
-    for scanner.Scan() {
-        if apiKey := strings.TrimPrefix(scanner.Text(), "OPENAI_API_KEY="); apiKey != scanner.Text() {
-            return apiKey
-        }
-    }
-    return ""
+func getApiKeyWithReaderWriter(reader io.Reader, writer io.Writer) string {
+	file, err := os.Open(".env")
+	if err != nil {
+		file, _ = os.Create(".env")
+	}
+	defer file.Close()
+	apiKey := ""
+	apiKey = getApiKeyFromScanner(file)
+	if apiKey == "" {
+		apiKey = getApiKeyFromInput(reader, writer, file)
+	}
+	return apiKey
+}
+
+func getApiKeyFromInput(reader io.Reader, writer io.Writer, file *os.File) string {
+	fmt.Fprintln(writer, "There is no .env file yet, so you need to supply an OpenAI API key.")
+	fmt.Fprintln(writer, "See: https://platform.openai.com/account/api-keys")
+	fmt.Fprint(writer, "Enter OpenAI API key: ")
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		apiKey := strings.TrimSpace(scanner.Text())
+		if apiKey != "" {
+			_, _ = file.WriteString(fmt.Sprintf("OPENAI_API_KEY=%s", apiKey))
+			return apiKey
+		}
+		fmt.Fprintln(writer, "Error: no API key provided")
+		fmt.Fprint(writer, "Enter OpenAI API key: ")
+	}
+	return ""
+}
+
+func getApiKeyFromScanner(reader io.Reader) string {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		// Only pay attention to appropriate line
+		apiKey := strings.TrimPrefix(scanner.Text(), "OPENAI_API_KEY=")
+		if apiKey != scanner.Text() {
+			return apiKey
+		}
+	}
+	return ""
 }
